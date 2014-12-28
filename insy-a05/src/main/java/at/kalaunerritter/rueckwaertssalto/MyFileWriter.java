5124d41d23f1e2c7531b25baddd76e76290b57d6
Paul Kalauner <pkalauner@student.tgm.ac.at>
2014-12-28 17:19:54 +0100

    comments!

--- insy-a05/src/main/java/at/kalaunerritter/rueckwaertssalto/MyFileWriter.java
+++ insy-a05/src/main/java/at/kalaunerritter/rueckwaertssalto/MyFileWriter.java
@@ -2,7 +2,6 @@ package at.kalaunerritter.rueckwaertssalto;
 
 import at.kalaunerritter.rueckwaertssalto.attributes.BaseAttribute;
 import at.kalaunerritter.rueckwaertssalto.attributes.ForeignKey;
-import at.kalaunerritter.rueckwaertssalto.attributes.PrimaryKey;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 
@@ -12,6 +11,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Collection;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 /**
@@ -50,21 +50,21 @@ public class MyFileWriter {
 
         for (Table cur : tables) {
             writer.print(cur.getTablename() + " (");
-            writer.println(collectionToString(cur.getAttributes()) + ")<br>");
+            writer.println(listToString(cur.getAttributes()) + ")<br>");
         }
         writer.flush();
     }
 
     /**
-     * Converts the content of a Collection to a String. <br>
+     * Converts the content of a List to a String. <br>
      * (We could also use the {@code toString()} method, but we do not want the [])
      *
-     * @param col Collection with BaseAttributes
+     * @param list List with BaseAttributes
      * @return String with the content of the given Collection
      */
-    private static String collectionToString(Collection<BaseAttribute> col) {
+    private static String listToString(List<BaseAttribute> list) {
         StringBuilder sb = new StringBuilder();
-        for (BaseAttribute cur : col)
+        for (BaseAttribute cur : list)
             sb.append(cur.getHTMLValue()).append(", ");
         return sb.substring(0, sb.lastIndexOf(", "));
     }
@@ -73,9 +73,9 @@ public class MyFileWriter {
     /**
      * writes the ERD to the file
      *
-     * @param tables the collection with the tables
+     * @param tables list with the tables
      */
-    public static void writeERDToFile(Collection<Table> tables) {
+    public static void writeERDToFile(List<Table> tables) {
         int counter = 0;
         Set<String> fkConnections = new HashSet<>();
 
@@ -87,23 +87,35 @@ public class MyFileWriter {
 
         gv.addln(gv.start_graph());
 
+        // Entitaeten generieren
         gv.add("node [shape=box]; ");
         for (Table table : tables) {
+            // Falls es sich um eine schwache Entitaet handelt, mit doppeltem Rahmen darstellen (peripheries=2)
             gv.add(table.getTablename() + (table.isWeak() ? " [peripheries=2]" : "") + "; ");
         }
         gv.addln();
 
+        // Attribute generieren
         gv.add("node [shape=ellipse]; ");
+        // Fuer jede Tabelle...
         for (Table table : tables) {
+            boolean weak = table.isWeak();
+            // durch alle Attribute jeder Tabelle iterieren
             for (BaseAttribute attr : table.getAttributes()) {
                 if (!(attr instanceof ForeignKey)) {
-                    gv.add("{node [label=\"" + attr.getValue() + ((attr instanceof PrimaryKey) ? " <PK>" : "") + "\"] " + attr.getValue() + counter + ";};");
+                    // Falls es sich nicht um einen Foreign Key handelt, stelle das Attribut dar
+                    gv.add("{node [label=<" + attr.getHTMLValue() + ">] " + attr.getValue() + counter + ";};");
+                    // Tabelle mit Attribut durch einen Strich verbinden
                     sbConnections.append(attr.getValue()).append(counter++).append(" -- ").append(table.getTablename()).append(";\n");
                 } else {
+                    // Falls es sich schon um einen Foreign Key handelt, erzeuge Relation
                     ForeignKey fk = (ForeignKey) attr;
                     String relationName = "\"" + table.getTablename() + "-" + fk.getForeignTable() + "\"";
-                    sbRelations.append(relationName).append(";");
-                    fkConnections.add(relationName + " -- " + table.getTablename() + " [label=\"1\",len=1.00];\n");
+                    // Falls es sich um einen Foreign Key handelt, stelle den Rahmen der Beziehung doppelt dar
+                    sbRelations.append(relationName).append(weak ? " [peripheries=2]" : "").append(";");
+                    // Relation mit Tabellen verbinden. Hierbei wird ein Set verwendet, um doppelte Verbindungen zu vermeiden
+                    // Falls es sich um einen Foreign Key handelt stelle die Verbindung mit doppelten Linien dar
+                    fkConnections.add(relationName + " -- " + table.getTablename() + " [label=\"1\",len=1.00" + (weak ? ",color=\"black:white:black\"" : "") + "];\n");
                     fkConnections.add(fk.getForeignTable() + " -- " + relationName + " [label=\"n\",len=1.00];\n");
                 }
             }
@@ -112,10 +124,13 @@ public class MyFileWriter {
         }
         gv.addln();
 
+        // Relationen (Diamanten) zum Dot-File hinzufuegen
         gv.add("node [shape=diamond,style=filled,color=lightgrey]; " + sbRelations.toString() + "\n");
 
+        // Verbindungslinien zum Dot-File hinzufuegen
         gv.add(sbConnections.toString());
 
+        // ForeignKey Verbindungen zum Dot-File hinzufuegen
         fkConnections.forEach(gv::add);
 
         gv.add(gv.end_graph());
@@ -126,6 +141,7 @@ public class MyFileWriter {
         LOG.info("Writing ERD to " + out.getAbsolutePath());
 
         try {
+            // In Dot File und falls Dot installiert ist, png erzeugen.
             gv.writeGraphToFile(gv.getGraph(gv.getDotSource(), "png"), out);
         } catch (DotNotInstalledException e) {
             LOG.info("Dot ist nicht installiert. Es konnte kein .png aus dem Dot-File generiert werden. Das tut uns leid.");
